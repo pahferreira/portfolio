3737676acf1b574ac243c7f243686df67488ccac
Pah Ferreira <pah.dsf@gmail.com>
2020-07-15 17:19:34 -0300

    Updates

---
+++ precache-manifest.da8134f8368711816eb1af7bbdab4ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46dd828905015f4f3b9bb0493ede8f70",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "7d9a93de8cae04e47acd",
+    "url": "/portfolio/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "c8862b76b9196cf8a351",
+    "url": "/portfolio/static/js/2.bd591e7a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/portfolio/static/js/2.bd591e7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d9a93de8cae04e47acd",
+    "url": "/portfolio/static/js/main.e989ae68.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
